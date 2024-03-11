bcce2d51e1e6c15058dd857545a544d6ef93aa04
Aidan Garcia <aidan.g0816@gmail.com>
2024-03-11 21:30:51 +0000

    Start

--- program.py
+++ program.py
@@ -1,1 +1,4 @@
-p
+Prolan_list["Java","Swift","Python","JavaScript","C++"]#/programming languages
+New_Prolan =input("what engine would you like to add?")
+def rank_Prolan(Prolan_list,New_Prolan):
+    for i in range(len(Prolan_list))
