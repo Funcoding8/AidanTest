e69fc9ee8ab2ba29d8281758cab4744e7d6260ed
Aidan Garcia <aidan.g0816@gmail.com>
2024-03-12 21:21:45 +0000

    what not work

--- program.py
+++ program.py
@@ -1,4 +1,20 @@
-Prolan_list["Java","Swift","Python","JavaScript","C++"]#/programming languages
-New_Prolan =input("what engine would you like to add?")
+Prolan_list = ["Java","Swift","Python","JavaScript","C++"]#/programming languages
+New_Prolan =input("what program would you like to add?")
 def rank_Prolan(Prolan_list,New_Prolan):
-    for i in range(len(Prolan_list))
+    for i in range(len(Prolan_list)):
+        rank = input("Do you like A)" + New_Prolan+ " more or B)" + Prolan_list[i] + "more? A/B")
+
+        if rank == "A":
+
+            Prolan_list.insert(i, New_Prolan) 
+            return Prolan_list
+
+        elif rank =="B":
+            Continue
+
+    Prolan_list.append(New_Prolan)
+
+    return Prolan_list
+
+print("Your new ranking of engines is", rank_Prolan (New_Prolan, Prolan_list))
+
